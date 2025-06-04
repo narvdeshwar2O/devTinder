ac61dec94b1a2df922e7eca4fe7b10466bf554df
Narvdeshwar <narvdeshwar@xenelsoft.co.in>
2025-06-04 16:42:45 +0530

    fix : ressolve the issue of duplicate email in signup

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -6,34 +6,43 @@ const User = require("../models/user.model.js");
 const authRouter = express.Router();
 
 authRouter.post("/login", async (req, res) => {
-  // console.log("login api called");
   const { email, password } = req.body;
+
   try {
-    const user = await User.findOne({ email: email });
-    console.log("current email user", user);
+    const user = await User.findOne({ email });
+
     if (!user) {
-      return res
-        .status(401)
-        .json({
-          messgae: "Email doesn't exits please create a new account.",
-          status: false,
-        });
+      return res.status(401).json({
+        message: "Email doesn't exist. Please create a new account.",
+        status: false,
+        data: null,
+      });
     }
+
     const isValidPassword = await user.verifyPassword(password);
+
     if (!isValidPassword) {
-      throw new Error("You have entered the wrong password !");
+      return res.status(401).json({
+        message: "You have entered the wrong password",
+        status: false,
+        data: null,
+      });
     }
+
     const token = await user.getJWT();
-    console.log(token);
-    // res.cookie("token", "ravi");
     res.cookie("token", token);
-    res.status(200).json({
+
+    return res.status(200).json({
       message: "User logged in successfully",
       status: true,
       data: user,
     });
   } catch (error) {
-    res.status(401).json({ message: "Unauthriosed user" });
+    return res.status(500).json({
+      message: error.message || "Something went wrong",
+      status: false,
+      data: null,
+    });
   }
 });
 
@@ -42,6 +51,13 @@ authRouter.post("/signup", async (req, res) => {
   try {
     validateSignup(req);
     const { firstName, email, password } = req.body;
+    // checking whether email is already registered
+
+    const isEmailAlreadyExits = await User.findOne({ email });
+    if (isEmailAlreadyExits) {
+      return res.status(400).json({ message: "Email already registered" });
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = new User({ firstName, email, password: hashedPassword });
     const data = await user.save();
